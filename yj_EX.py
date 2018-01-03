73e32068664ff5957ad2cdc601a413abf54b089a
YJ <18298405849@163.com>
2018-01-03 12:06:12 +0800

    修改yj_accountList接口函数里面的数据

--- yj_EX.py
+++ yj_EX.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -166,13 +167,12 @@ def yj_accountList():
 
                         },
                         'to': {
-                            'userLogo': '../static/images/transferAccounts/user1.png',
-                            'way': '支付宝',
-                            'username': '杨娇',
-                            'classify': '现金账户',
-                            'use': '话费',
-                            'money': 1000,
-                            'time': '2017-12-24 11:00'
+                            'account_user': '../static/images/accountList/user_logo1.png',
+                            'account_way': '支付宝',
+                            'account_username': '杨娇',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 1000
                         }
                     },
                     {
@@ -181,13 +181,12 @@ def yj_accountList():
 
                         },
                         'to': {
-                            'userLogo': '../static/images/transferAccounts/user2.png',
-                            'way': '现金',
-                            'username': '卢鹏',
-                            'classify': '现金账户',
-                            'use': '圣诞礼物',
-                            'money': 2000,
-                            'time': '2017-12-24 12:00'
+                            'account_user': '../static/images/accountList/user_logo2.png',
+                            'account_way': '现金',
+                            'account_username': '卢鹏',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 2000
                         }
                     },
                     {
@@ -196,13 +195,12 @@ def yj_accountList():
 
                         },
                         'to': {
-                            'userLogo': '../static/images/transferAccounts/user3.png',
-                            'way': '白条',
-                            'username': '杨娇',
-                            'classify': '京东白条',
-                            'use': '买烤鸭',
-                            'money': 200,
-                            'time': '2017-12-24 11:00'
+                            'account_user': '../static/images/accountList/user_logo3.png',
+                            'account_way': '支付宝',
+                            'account_username': '杨娇',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 4000
                         }
                     },
                     {
@@ -211,13 +209,54 @@ def yj_accountList():
 
                         },
                         'to': {
-                            'userLogo': '../static/images/transferAccounts/user4.png',
-                            'way': '支付宝',
-                            'username': '卢鹏',
-                            'classify': '麦宝账户',
-                            'use': '话费',
-                            'money': 100,
-                            'time': '2017-12-24 13:00'
+                            'account_user': '../static/images/accountList/user_logo4.png',
+                            'account_way': '支付宝',
+                            'account_username': 'LP',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 100
+                        }
+                    },
+                    {
+                        'id': 10005,
+                        'from': {
+
+                        },
+                        'to': {
+                            'account_user': '../static/images/accountList/user_logo5.png',
+                            'account_way': '支付宝',
+                            'account_username': 'YANGJIAO',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 400
+                        }
+                    },
+                    {
+                        'id': 10006,
+                        'from': {
+
+                        },
+                        'to': {
+                            'account_user': '../static/images/accountList/user_logo6.png',
+                            'account_way': '支付宝',
+                            'account_username': 'LIZE',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 800
+                        }
+                    },
+                    {
+                        'id': 10007,
+                        'from': {
+
+                        },
+                        'to': {
+                            'account_user': '../static/images/accountList/user_logo7.png',
+                            'account_way': '支付宝',
+                            'account_username': 'LP',
+                            'account_classify': '现金账户',
+                            'account_datetime': '2017-12-24 11:00',
+                            'account_money': 1000
                         }
                     }
                 ]
